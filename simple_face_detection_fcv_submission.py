fddd94592f44dca2b034d59fd08aea21803aba31
JoaoMoranguinho <joao.moranguinho13@gmail.com>
2019-07-08 12:56:07 +0100

    red_eyes_detection
    inicial

--- simple_face_detection_fcv_submission.py
+++ simple_face_detection_fcv_submission.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
 import os
+import matplotlib.pyplot as plt
 
 # Load the Haar cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -28,6 +29,7 @@ def detect(gray, frame):
         roi_color = frame[y:y+h, x:x+w]
         eyes = eyes_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
+            eye_roi(roi_color, ex, ey, ew, eh)
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
             cv2.imshow('img',frame)
     cv2.waitKey(0)
@@ -53,6 +55,16 @@ def detect(gray, frame):
             eye_center[1] = (0,0)
     return eye_center[0], eye_center[1], frame
 
+def eye_roi(region, ex, ey, ew, eh):
+    roi = region[ey:ey+eh, ex:ex+ew]
+    for i in range(0, eh):
+        for j in range(0,ew):
+            pixel = roi[i,j]
+            if(150 < pixel[2] < 255 and pixel[0] < 100 and pixel[1] < 100):
+                roi[i,j] = [255,255,0]
+
+    return None
+
 if(len(sys.argv) != 3):
     print('Check.exe <faceimagefile> <outputfile>')
     sys.exit(0)
@@ -71,28 +83,6 @@ if(left_eye_center_x > right_eye_center_x):
     ((left_eye_center_x, left_eye_center_y), (right_eye_center_x, right_eye_center_y)) = \
         ((right_eye_center_x, right_eye_center_y), (left_eye_center_x, left_eye_center_y))
 
-
-'''
-    Each line of the outputfile must contain the following fields separated by a blank space character:
-    ImageName is the image file name (e.g., image1.png).
-    RetVal is an integer value indicating if the input image can be processed or not by the executable3:
-    1 if the image can be processed.
-    0 if the image cannot be processed and no more information is available.
-    -1 if the image cannot be processed due to unsupported image size.
-    -2 if the image cannot be processed due to unsupported image format.
-    -3 if the image cannot be processed due to unuseful image content.
-    LE_x is an integer value indicating the X coordinate (in pixels) of the left eye center4.
-    LE_y is an integer value indicating the Y coordinate (in pixels) of the left eye center4.
-    RE_x is an integer value indicating the X coordinate (in pixels) of the right eye center4.
-    RE_y is an integer value indicating the Y coordinate (in pixels) of the right eye center4.
-    Test_2 is an integer value in the range [0;100] indicating the compliance degree of the input image with respect to the Compliance Test 2. 0 means no compliancy, 100 maximum compliancy. Moreover, the following special characters have been defined:
-    ‘-‘ if the executable is not able to evaluate this requirement.
-    ‘?’ if, usually, the executable is able to evaluate this requirement but it was not able to evaluate it on the current input image for an identified failure (e.g., the SDK is not able to evaluate the presence of red eyes since the eyes are closed).
-    ‘!’ if, usually, the executable is able to evaluate this requirement but it was not able to evaluate it on the current input image for an unknown failure.
-    Test_3 analogous to test 2 but related to test 3.
-    ...
-    Test_24 analogous to test 2 but related to test 24.
-    '''
 file = open(result_file_name,'a')
 
 result = os.path.basename(image_file_name) + " 1 " + str(left_eye_center_x) + " " + str(left_eye_center_y) + " " + \
