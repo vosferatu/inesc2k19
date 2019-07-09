6f97a553220e1c758b483b55d6dad545dd788d6d
JoaoMoranguinho <joao.moranguinho13@gmail.com>
2019-07-09 13:32:01 +0100

    area reduzida e output adicionado

--- simple_face_detection_fcv_submission.py
+++ simple_face_detection_fcv_submission.py
@@ -3,6 +3,9 @@ import sys
 import os
 import matplotlib.pyplot as plt
 
+# Define tests array
+tests = []
+
 # Load the Haar cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -22,21 +25,50 @@ def detect(gray, frame):
     # 1.3 is the kernel size or size of image reduced when applying the detection
     # 5 is the number of neighbors after which we accept that is a face
 
-    #visualize de rectangles
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    # visualize de rectangles
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eyes_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            eye_roi(roi_color, ex, ey, ew, eh)
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            cv2.imshow('img',frame)
+        eyes_red_percentages = []
+        for (ex, ey, ew, eh) in eyes:
+            eyes_red_percentages.append(red_eye_test(roi_color, ex, ey, ew, eh))
+            centre = (ex+(ew/2), ey+(eh/2))
+            new_ex = centre[0] - (ew/4)
+            new_ey = centre[1] - (eh/4)
+            cv2.rectangle(roi_color, (new_ex, new_ey), (new_ex+ew/2, new_ey+eh/2), (0, 255, 0), 2)
+            cv2.imshow('img', frame)
+        mean = sum(eyes_red_percentages)/2
+        value = 0
+        if(mean < 0.02):
+            value = 100
+        elif(mean < 0.04):
+            value = 90
+        elif(mean < 0.05):
+            value = 80
+        elif(mean < 0.06):
+            value = 70
+        elif(mean < 0.07):
+            value = 60
+        elif(mean < 0.08):
+            value = 50
+        elif(mean < 0.09):
+            value = 40
+        elif(mean < 0.1):
+            value = 30
+        elif(mean < 0.2):
+            value = 20
+        elif(mean < 0.3):
+            value = 10
+        elif(mean < 0.4):
+            value = 0
+        tests.append(["Test_14", value])
     cv2.waitKey(0)
 
     # Now iterate over the faces and detect eyes
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         # Arguements => image, top-left coordinates, bottomright coordinates, color, rectangle border thickness
 
         # we now need two region of interests(ROI) grey and color for eyes one to detect and another to draw rectangle
@@ -44,26 +76,50 @@ def detect(gray, frame):
         roi_color = frame[y:y+h, x:x+w]
         # Detect eyes now
         eyes = eyes_cascade.detectMultiScale(roi_gray, 1.1, 3)
-        
+
         eye_center = {}
-        
+
         for counter, (ex, ey, ew, eh) in enumerate(eyes):
-            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
             eye_center[counter] = (ex+(ew/2), ey+(eh/2))
         if(len(eye_center) < 2):
-            eye_center[0] = (0,0)
-            eye_center[1] = (0,0)
+            eye_center[0] = (0, 0)
+            eye_center[1] = (0, 0)
     return eye_center[0], eye_center[1], frame
 
-def eye_roi(region, ex, ey, ew, eh):
+def red_eye_test(region, ex, ey, ew, eh):
+    pixel_counter = 0.00
+    red_pixel_counter = 0.00
+    red_percentage = 0.00
+    centre = (ex+(ew/2), ey+(eh/2))
+    new_ex = centre[0] - (ew/4)
+    new_ey = centre[1] - (eh/4)
+    roi = region[new_ey:ey+eh, new_ex:ex+ew]
+    for i in range(0, eh/2):
+        for j in range(0, ew/2):
+            pixel_counter += 1
+            pixel = roi[i, j]
+            if(130 < pixel[2] < 255 and pixel[0] < 100 and pixel[1] < 100):
+                roi[i, j] = [255, 255, 0]
+                red_pixel_counter += 1
+    red_percentage = red_pixel_counter/pixel_counter
+    return red_percentage
+
+def red_eye_test_old(region, ex, ey, ew, eh):
+    pixel_counter = 0.00
+    red_pixel_counter = 0.00
+    red_percentage = 0.00
     roi = region[ey:ey+eh, ex:ex+ew]
     for i in range(0, eh):
-        for j in range(0,ew):
-            pixel = roi[i,j]
+        for j in range(0, ew):
+            pixel_counter += 1
+            pixel = roi[i, j]
             if(150 < pixel[2] < 255 and pixel[0] < 100 and pixel[1] < 100):
-                roi[i,j] = [255,255,0]
+                roi[i, j] = [255, 255, 0]
+                red_pixel_counter += 1
+    red_percentage = red_pixel_counter/pixel_counter
+    return red_percentage
 
-    return None
 
 if(len(sys.argv) != 3):
     print('Check.exe <faceimagefile> <outputfile>')
@@ -77,21 +133,25 @@ frame = cv2.imread(image_file_name)
 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-(left_eye_center_x, left_eye_center_y), (right_eye_center_x, right_eye_center_y), canvas = detect(gray, frame)
+(left_eye_center_x, left_eye_center_y), (right_eye_center_x,
+                                         right_eye_center_y), canvas = detect(gray, frame)
 
 if(left_eye_center_x > right_eye_center_x):
     ((left_eye_center_x, left_eye_center_y), (right_eye_center_x, right_eye_center_y)) = \
-        ((right_eye_center_x, right_eye_center_y), (left_eye_center_x, left_eye_center_y))
+        ((right_eye_center_x, right_eye_center_y),
+         (left_eye_center_x, left_eye_center_y))
 
-file = open(result_file_name,'a')
+file = open(result_file_name, 'a')
 
 result = os.path.basename(image_file_name) + " 1 " + str(left_eye_center_x) + " " + str(left_eye_center_y) + " " + \
-            str(right_eye_center_x) + " " + str(right_eye_center_y)
-
-for test in range(2,25):
-    result = result + " - "
+    str(right_eye_center_x) + " " + str(right_eye_center_y)
 
 file.write(result)
+file.write("\n")
+
+for test in tests:
+    file.write(test[0] + " " + str(test[1]))
+
 file.write("\n")
 
 file.close()
